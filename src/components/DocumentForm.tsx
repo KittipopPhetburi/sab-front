f4e67c036b5f858159d8a5164409b60c29de85af
pratimakornyok <nayokjojo@gmail.com>
2025-11-26 13:12:36 +0700

    test

--- src/components/DocumentForm.tsx
+++ src/components/DocumentForm.tsx
@@ -19,7 +19,7 @@ import {
   TableHeader,
   TableRow,
 } from "./ui/table";
-import { Plus, Trash2 } from "lucide-react";
+import { HardDriveDownload, Plus, Trash2 } from "lucide-react";
 import {
   Command,
   CommandEmpty,
@@ -281,7 +281,7 @@ export default function DocumentForm({
                 <Command className="bg-transparent shadow-none rounded-none m-0 p-2">
                   <CommandInput placeholder="ค้นหาลูกค้า/คู่ค้า..." className="bg-transparent rounded-md border px-2" />
                   <CommandList>
-                    <CommandEmpty>ไม่พบข้อมูล</CommandEmpty>
+                    <CommandEmpty>ไม่พบข้อมูล</CommandEmpty> 
                     <CommandGroup>
                       {activeCustomers.map((customer) => (
                         <CommandItem
@@ -493,7 +493,7 @@ export default function DocumentForm({
                 rows={6}
               />
             </div>
-
+            {/* test push*/ }
             {/* Summary */}
             <div className="space-y-3">
               <div className="flex justify-between items-center">
